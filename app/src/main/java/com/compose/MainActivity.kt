a13bc710f58ac3e711052fd0c31d7419158de87b
dyddn3994 <dyddn3994@naver.com>
2023-07-19 13:50:58 +0900

    또 테스트 바디? 바디바디?

--- app/src/main/java/com/compose/MainActivity.kt
+++ app/src/main/java/com/compose/MainActivity.kt
@@ -38,7 +38,6 @@ class MainActivity : ComponentActivity() {
                         moveActivityBtn({ startActivity(Intent(context, ButtonActivity::class.java)) }, "Button")
                         moveActivityBtn({ startActivity(Intent(context, CheckboxActivity::class.java)) }, "Checkbox")
                         moveActivityBtn({ startActivity(Intent(context, SnackbarActivity::class.java)) }, "Snackbar")
-                        // test
                     }
                 }
             }
